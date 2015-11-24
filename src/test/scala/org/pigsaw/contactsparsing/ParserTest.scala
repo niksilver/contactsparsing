148c1ebd9e3c00d5139ef63b8c440428d5ac61ae
Nik Silver <nik@niksilver.com>
2015-11-24 22:00:26 +0000

    Initial import. A dummy test passes.

---
+++ src/test/scala/org/pigsaw/contactsparsing/ParserTest.scala
@@ -0,0 +1,14 @@
+package org.pigsaw.contactsparsing
+
+import org.scalatest.{ShouldMatchers, FunSpec}
+
+/**
+  * Created by Nik on 24 Nov 2015.
+  */
+class ParserTest extends FunSpec with ShouldMatchers {
+  describe("Hello") {
+    it ("should know 1+1 = 2") {
+      (1+1) should equal (2)
+    }
+  }
+}
