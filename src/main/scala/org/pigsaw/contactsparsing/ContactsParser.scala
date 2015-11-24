3bea421cfcbe69e5642de3fd1a30dd3d8a226f6d
Nik Silver <nik@niksilver.com>
2015-11-24 22:35:05 +0000

    Added basic parsing (and tests).

---
+++ src/main/scala/org/pigsaw/contactsparsing/ContactsParser.scala
@@ -0,0 +1,11 @@
+package org.pigsaw.contactsparsing
+
+import scala.util.parsing.combinator.RegexParsers
+
+/**
+  * Created by Nik on 24 Nov 2015.
+  */
+class ContactsParser extends RegexParsers {
+
+  def simpleField = """[^,"]*""".r
+}
--- src/test/scala/org/pigsaw/contactsparsing/ParserTest.scala
+++ src/test/scala/org/pigsaw/contactsparsing/ParserTest.scala
@@ -2,13 +2,52 @@ package org.pigsaw.contactsparsing
 
 import org.scalatest.{ShouldMatchers, FunSpec}
 
+import scala.util.Success
+import scala.util.parsing.combinator.Parsers
+
 /**
   * Created by Nik on 24 Nov 2015.
   */
 class ParserTest extends FunSpec with ShouldMatchers {
-  describe("Hello") {
-    it ("should know 1+1 = 2") {
-      (1+1) should equal (2)
+
+  trait TestParser extends ContactsParser {
+    def parseOption[T](parser: Parser[T], input: String): Option[T] =
+      parseAll(parser, input) match {
+        case Success(out, _) => Some(out)
+        case fail => None
+      }
+  }
+
+  describe("simpleField") {
+    it ("should know a simple string is a field") {
+      new TestParser {
+        parseOption(simpleField, "hello") should equal(Some("hello"))
+        parseOption(simpleField, "byeee") should equal(Some("byeee"))
+      }
+    }
+
+    it ("should accept empty string as a field") {
+      new TestParser {
+        parseOption(simpleField, "") should equal (Some(""))
+      }
+    }
+
+    it ("should accept a field with dots and spaces and alphanumerics") {
+      new TestParser {
+        parseOption(simpleField, "dot... dot... dot...") should equal (Some("dot... dot... dot..."))
+      }
+    }
+
+    it ("should reject a string with a comma") {
+      new TestParser {
+        parseOption(simpleField, "aye,aye") should equal (None)
+      }
+    }
+
+    it ("should reject a string with a double quote") {
+      new TestParser {
+        parseOption(simpleField, """aye "!" aye""") should equal (None)
+      }
     }
   }
 }
